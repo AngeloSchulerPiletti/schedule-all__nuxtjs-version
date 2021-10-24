486b06264d2d65cde066e1c67d7910821ed87fa5
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2021-10-24 18:29:35 -0300

    nuxt installed

---
+++ frontend/pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  computed:{
+    counter(){
+      return this.$store.state.counter;
+    }
+  },
+  methods:{
+    incrementState(){
+      this.$store.commit('increment');
+    }
+  }
+}
+</script>
