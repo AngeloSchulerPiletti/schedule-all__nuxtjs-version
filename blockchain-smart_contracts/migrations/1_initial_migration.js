9cc887b2f676f74c402e24548da6ead20f8e2725
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-01-17 09:44:57 -0300

    migrating the blockchain from schedule-all__nuxtjs-version to schedule-all_blockchain-network repo

--- blockchain-smart_contracts/migrations/1_initial_migration.js
+++
@@ -1,7 +0,0 @@
-const Migrations = artifacts.require("Migrations");
-const TaskToken = artifacts.require("TaskToken");
-
-module.exports = function (deployer) {
-  deployer.deploy(Migrations);
-  deployer.deploy(TaskToken);
-};
