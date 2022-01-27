b62b9230e89f83faadd9b1d52ac9593f5a353d41
Angelo Schuler Piletti <74366173+AngeloSchulerPiletti@users.noreply.github.com>
2022-01-26 21:52:12 -0300

    first wallet page feature

---
+++ frontend/components/templates/Wallet/WalletStatus.vue
@@ -0,0 +1,110 @@
+<template>
+  <div id="wallet-status-container" class="flex_c wrapper border-soft">
+    <h6 class="upper flex_r">
+      Status da wallet <connection :class="`status-${isWalletSigned}`" />
+    </h6>
+    <div id="explanation-container" class="flex_c up">
+      <div :class="`status-container status-${isWalletSigned}`">
+        <p v-if="isWalletSigned">
+          A wallet conectada é a wallet que você utilizou no seu cadastro.
+          Aproveite todas as funcionalidades do aplicativo!
+        </p>
+        <p v-else-if="isWalletSigned === null">
+          Você precisa conectar sua wallet para a aplicação funcionar
+          corretamente
+        </p>
+        <p v-else>
+          A wallet conectada <b>não</b> é a wallet que você utilizou no seu
+          cadastro. Sugerimos que você se conecte a ela para poder aproveitar
+          todas as funcionalidades do aplicativo. <br />
+          Acreditamos que a wallet do seu cadastro seja:
+          <b>{{ getSignedWallet }}</b>
+        </p>
+      </div>
+      <div class="actions flex_r">
+        <button class="button-3 btn-4_tiny">Atualizar</button>
+        <button
+          :class="!isWalletConnected ? 'button-1 btn-4_tiny' : 'button-disabled button-1 btn-4_tiny'"
+          @click="connectWallet"
+        >
+          Conectar
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { connectWallet } from '@/utils/walletConnectionManager.js'
+import Connection from '@/components/icons/Connection.vue'
+
+export default {
+  computed: {
+    isWalletConnected() {
+      return this.$store.state.wallet.connected
+    },
+    isWalletSigned() {
+      return this.$store.state.wallet.isTheConnectedWalletSigned
+    },
+    getSignedWallet() {
+      return this.$cookies.get('userData').walletAddress
+    },
+  },
+  methods: {
+    async connectWallet() {
+      await connectWallet(this.$userWeb3, this.$store, this.$cookies)
+    },
+  },
+  components: {
+    Connection,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+#wallet-status-container {
+  margin: 20px 30px 30px 20px;
+  padding: 20px;
+  gap: 22px;
+
+  &::before {
+    border-radius: 8px;
+    box-shadow: 6px 6px 14px #a9a8b7, -6px -6px 14px #ffffff,
+      inset -4px -4px 14px #ffffff, inset 4px 4px 14px #a9a8b7;
+  }
+
+  h6 {
+    gap: 8px;
+    align-items: center;
+    svg {
+      width: 0.85em;
+      height: 0.85em;
+      &::v-deep {
+        & path {
+          fill: $check_red;
+        }
+        &.status-true path {
+          fill: $check_green;
+        }
+      }
+    }
+  }
+  #explanation-container {
+    gap: 16px;
+
+    .status-container {
+      font-size: 14px;
+      line-height: 1.4em;
+      padding-left: 6px;
+      border-left: 2px solid $check_red;
+
+      &.status-true {
+        border-left: 2px solid $check_green;
+      }
+    }
+    .actions {
+        gap: 10px;
+    }
+  }
+}
+</style>
--- frontend/pages/schedule/wallet.vue
+++ frontend/pages/schedule/wallet.vue
@@ -1,16 +1,24 @@
 <template>
-  
+  <div>
+    <wallet-status/>
+  </div>
 </template>
 
 <script>
+import WalletStatus from "@/components/templates/Wallet/WalletStatus.vue";
+
 export default {
   layout: 'dashboard',
-  head:{
-    title: "Wallet",
+  head: {
+    title: 'Wallet',
+  },
+  mounted() {
+    this.$store.commit('setDashboardPageStatus', 'loaded')
+  },
+  components:{
+    WalletStatus,
   },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
